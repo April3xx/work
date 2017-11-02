c6c4dbcb5de169df10b4234d006b0b38a33708fc
sode20yrs <31275335+sode20yrs@users.noreply.github.com>
2017-11-02 16:31:10 +0700

    list

--- MyApplication/app/src/main/java/com/example/priva/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/priva/myapplication/MainActivity.java
@@ -4,57 +4,41 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    EditText text;
-    RadioButton usbutton, jpbutton;
-    Button btn, btn2;
-    float rate = 0;
-    final float usrate = 40;
-    final float jprate = 30;
-
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text = (EditText) findViewById(R.id.editText);
-        usbutton = (RadioButton) findViewById(R.id.radioButton);
-        jpbutton = (RadioButton) findViewById(R.id.radioButton2);
-        btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(this);
-        btn2 = (Button) findViewById(R.id.button2);
-        btn2.setOnClickListener(this);
+        String[] movies = {"An American Horror Story Season 1",
+                "An American Horror Story Season 1",
+                "An American Horror Story Season 1",
+                "Game of Thrones Season 1",
+                "Game of Thrones Season 2",
+                "Game of Thrones Season 3",
+                "Game of Thrones Season 4",
+                "True Detective Season 1"
+        };
+        ListAdapter listviewadapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,movies);
+        ListView listView =(ListView) findViewById(R.id.listview);
+        listView.setAdapter(listviewadapter);
+        listView.setOnItemClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        if (usbutton.isChecked()) {
-            float input = Float.parseFloat(text.getText().toString());
-            rate = input * usrate;
-        }
-        if (jpbutton.isChecked()) {
-            float input = Float.parseFloat(text.getText().toString());
-            rate = input * jprate;
-        }
-        switch (v.getId()) {
-            case R.id.button:
-                text.setText(String.valueOf(rate));
-                break;
-            case R.id.button2:
 
-                String s = Float.toString(rate);
-                Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(this, Answer.class);
-                i.putExtra("rate", s);
-                startActivity(i);
-                break;
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+            String movies = String.valueOf(parent.getItemAtPosition(position));
+            Toast.makeText(this,movies,Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this,Show.class);
+            i.putExtra("movies",movies);
+            startActivity(i);
 
-        }
     }
 }
